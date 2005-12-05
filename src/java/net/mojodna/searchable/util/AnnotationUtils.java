2310bc086aa0222f7b8fe10e62ab3d001690f45c
seth <seth@43f9e6a5-3c06-0410-9c50-a813a83a72d6>
2005-12-05 23:00:55 +0000

    check all inherited interfaces rather than just those present on the declaring class use getAnnotation() to implement isAnnotationPresent()
    git-svn-id: http://svn.mojodna.net/repository/trunk/searchable@76 43f9e6a5-3c06-0410-9c50-a813a83a72d6

--- src/java/net/mojodna/searchable/util/AnnotationUtils.java
+++ src/java/net/mojodna/searchable/util/AnnotationUtils.java
@@ -2,13 +2,54 @@ package net.mojodna.searchable.util;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-
-import org.apache.log4j.Logger;
-
-import net.mojodna.searchable.Searchable.Indexed;
+import java.util.Collection;
+import java.util.HashSet;
 
 public class AnnotationUtils {
-    private static final Logger log = Logger.getLogger( AnnotationUtils.class );
+    /**
+     * Get a specific annotation present on a method.
+     * 
+     * This differs from AnnotatedElement.getAnnotation(Annotation) in that it
+     * looks up the class hierarchy for inherited annotations.  (@Inherit only
+     * applies to class-level annotations.)  It also checks declarations within
+     * interfaces.
+     * 
+     * @see java.lang.reflect.AnnotatedElement#getAnnotations()
+     * 
+     * @param method Method to check for present annotations.
+     * @param annotation Annotation to look for.
+     * @return Instance of the specified annotation or null if not present.
+     */
+    public static Annotation getAnnotation(final Method method, final Class<? extends Annotation> annotation) {
+        if ( null == method || null == annotation )
+            return null;
+
+        Class clazz = method.getDeclaringClass();
+        
+        final Collection<Class> classesToCheck = new HashSet();
+        while ( null != clazz ) {
+            classesToCheck.add( clazz );
+            
+            // add implemented interfaces to the list of classes to check
+            for ( final Class iface : clazz.getInterfaces() ) {
+                classesToCheck.add( iface );
+            }
+            
+            clazz = clazz.getSuperclass();
+        }
+        
+        // check all superclasses and inherited interfaces
+        for ( final Class c : classesToCheck ) {
+            try {
+                final Method m = c.getMethod( method.getName(), (Class[]) method.getParameterTypes() );
+                if ( m.isAnnotationPresent( annotation ) )
+                    return m.getAnnotation( annotation );
+            }
+            catch (final NoSuchMethodException e) {}
+        }
+        
+        return null;
+    }
     
     /**
      * Determine whether a method (or methods that it overrides) are annotated
@@ -26,92 +67,6 @@ public class AnnotationUtils {
      * @return Whether the specified annotation is present on a given method.
      */
     public static boolean isAnnotationPresent(final Method method, final Class<? extends Annotation> annotation) {
-        if ( null == method || null == annotation )
-            return false;
-        
-        Class clazz = method.getDeclaringClass();
-        
-        // check implemented interfaces
-        for ( final Class iface : clazz.getInterfaces() ) {
-            try {
-                final Method m = iface.getMethod( method.getName(), (Class[]) method.getParameterTypes() );
-                if ( m.isAnnotationPresent( annotation ) )
-                    return true;
-            }
-            catch (final NoSuchMethodException e) {}
-        }
-        
-        // check all declarations, in this class or in superclasses
-        while ( null != clazz ) {
-            try {
-                final Method m = clazz.getMethod( method.getName(), (Class[]) method.getParameterTypes() );
-                if ( m.isAnnotationPresent( annotation ) )
-                    return true;
-            }
-            catch (final NoSuchMethodException e) {
-                // no method declared or inherited here
-                break;
-            }
-            // check the superclass for a method with an appropriate annotation
-            clazz = clazz.getSuperclass();
-        }
-        
-        return false;
-    }
-    
-    class Parent {
-        @Indexed
-        public String getName() {
-            return "rick";
-        }
-        @Indexed
-        public String getColor() {
-            return "blue";
-        }
-    }
-    
-    class Child extends Parent {
-        public String getName() {
-            return "seth";
-        }
-        public String getSky() {
-            return "orange";
-        }
-    }
-    
-    class Grandchild extends Child {
-        public String getColor() {
-            return "green";
-        }
-        @Indexed
-        public String getSky() {
-            return "vanilla";
-        }
-    }
-    
-    public static void main(final String[] args) {
-        final AnnotationUtils x = new AnnotationUtils();
-        
-        log.debug("=== Parent ===");
-        final AnnotationUtils.Parent p = x.new Parent();
-        for ( final Method m : p.getClass().getMethods() ) {
-            if ( AnnotationUtils.isAnnotationPresent( m, Indexed.class ) )
-                log.debug( m.getName() );
-        }
-
-        log.debug("=== Child ===");
-        final AnnotationUtils.Child c = x.new Child();
-        for ( final Method m : c.getClass().getMethods() ) {
-            if ( AnnotationUtils.isAnnotationPresent( m, Indexed.class ) )
-                log.debug( m.getName() );
-        }
-
-        log.debug("=== Grandchild ===");
-        final AnnotationUtils.Grandchild gc = x.new Grandchild();
-        for ( final Method m : gc.getClass().getMethods() ) {
-            if ( AnnotationUtils.isAnnotationPresent( m, Indexed.class ) )
-                log.debug( m.getName() );
-        }
-
+        return ( null != getAnnotation( method, annotation ) ); 
     }
 }
