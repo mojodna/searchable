1f96ba5b24834f752d5b715743a163fd362e243f
seth <seth@43f9e6a5-3c06-0410-9c50-a813a83a72d6>
2005-12-07 22:10:29 +0000

    ResultSet interface
    git-svn-id: http://svn.mojodna.net/repository/trunk/searchable@115 43f9e6a5-3c06-0410-9c50-a813a83a72d6

--- src/java/net/mojodna/searchable/ResultSet.java
+++ src/java/net/mojodna/searchable/ResultSet.java
@@ -3,54 +3,11 @@ package net.mojodna.searchable;
 import java.util.Collection;
 import java.util.Iterator;
 
-public class ResultSet implements Iterable<Result> {
-	private int count;
-	private int offset;
-	private Collection<Result> results;
-	private int size;
-	
-	ResultSet(final int size) {
-		super();
-		setSize( size );
-	}
-	
-	public int count() {
-		return count;
-	}
-	
-	void setCount(final int count) {
-		this.count = count;
-	}
-	
-	public int offset() {
-		return offset;
-	}
-	
-	void setOffset(final int offset) {
-		this.offset = offset;
-	}
-	
-	public Collection<Result> getResults() {
-		return results;
-	}
-	
-	void setResults(final Collection<Result> results) {
-		this.results = results;
-	}
-	
-	public int size() {
-		return size;
-	}
-	
-	void setSize(final int size) {
-		this.size = size;
-	}
-	
-	public boolean isEmpty() {
-		return results.isEmpty();
-	}
-	
-	public Iterator<Result> iterator() {
-		return results.iterator();
-	}
+public interface ResultSet extends Iterable<Result> {
+	public int count();
+	public int offset();
+	public Collection<Result> getResults();
+	public int size();
+	public boolean isEmpty();
+	public Iterator<Result> iterator();
 }
