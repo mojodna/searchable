a8e6a60468722ca76a2a936587140cbe01316fc8
seth <seth@43f9e6a5-3c06-0410-9c50-a813a83a72d6>
2006-02-16 16:36:48 +0000

    updated copyright notice to include 2006 and changed seth's email address
    git-svn-id: http://svn.mojodna.net/repository/trunk/searchable@173 43f9e6a5-3c06-0410-9c50-a813a83a72d6

--- src/java/net/mojodna/searchable/AbstractResult.java
+++ src/java/net/mojodna/searchable/AbstractResult.java
@@ -1,5 +1,5 @@
 /*
-Copyright 2005      Seth Fitzsimmons <seth@prx.org>
+Copyright 2005-2006 Seth Fitzsimmons <seth@note.amherst.edu>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
