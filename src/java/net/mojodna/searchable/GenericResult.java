100f474a4a92fd0306d412d45adf7cac9bb617dd
seth <seth@43f9e6a5-3c06-0410-9c50-a813a83a72d6>
2006-02-21 23:46:29 +0000

    lean on AbstractResult
    git-svn-id: http://svn.mojodna.net/repository/trunk/searchable@184 43f9e6a5-3c06-0410-9c50-a813a83a72d6

--- src/java/net/mojodna/searchable/GenericResult.java
+++ src/java/net/mojodna/searchable/GenericResult.java
@@ -15,16 +15,11 @@ limitations under the License.
 */
 package net.mojodna.searchable;
 
-import java.util.Map;
 
-public class GenericResult implements Result {
+public class GenericResult extends AbstractResult implements Result {
     private String id;
     private String type;
-    
-    private int ranking;
-    private Map storedFields;
-    private float score;
-    
+
     public String getId() {
         return id;
     }
@@ -39,29 +34,5 @@ public class GenericResult implements Result {
     
     void setType(final String type) {
         this.type = type;
-    }
-    
-    public int getRanking() {
-        return ranking;
-    }
-    
-    public void setRanking(final int ranking) {
-        this.ranking = ranking;
-    }
-    
-    public Map getStoredFields() {
-        return storedFields;
-    }
-    
-    public void setStoredFields(final Map storedFields) {
-        this.storedFields = storedFields;
-    }
-    
-    public float getScore() {
-        return score;
-    }
-    
-    public void setScore(final float score) {
-        this.score = score;
     }
 }
