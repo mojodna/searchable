e88a789f6cf1f71b3a4443992522042e22ec72e6
seth <seth@43f9e6a5-3c06-0410-9c50-a813a83a72d6>
2006-09-29 21:59:09 +0000

    SolrSearcher stub
    git-svn-id: http://svn.mojodna.net/repository/searchable/trunk@687 43f9e6a5-3c06-0410-9c50-a813a83a72d6

---
+++ src/main/java/net/mojodna/searchable/solr/SolrSearcher.java
@@ -0,0 +1,32 @@
+/*
+ Copyright 2005-2006 Seth Fitzsimmons <seth@mojodna.net>
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+package net.mojodna.searchable.solr;
+
+import net.mojodna.searchable.ResultSet;
+import net.mojodna.searchable.Searchable;
+import net.mojodna.searchable.Searcher;
+
+/**
+ * Queries a Solr server for results.
+ * 
+ * @author Seth Fitzsimmons
+ */
+public class SolrSearcher implements Searcher<Searchable> {
+	public ResultSet search(final String query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
