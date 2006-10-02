ee98ec5c49745018cb68cef9f275d6048ea6df79
seth <seth@43f9e6a5-3c06-0410-9c50-a813a83a72d6>
2006-10-02 15:29:27 +0000

    cleanup
    git-svn-id: http://svn.mojodna.net/repository/searchable/trunk@692 43f9e6a5-3c06-0410-9c50-a813a83a72d6

--- src/main/java/net/mojodna/searchable/solr/SolrSearcher.java
+++ src/main/java/net/mojodna/searchable/solr/SolrSearcher.java
@@ -45,8 +45,6 @@ import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
-import org.jdom.output.Format;
-import org.jdom.output.XMLOutputter;
 
 /**
  * Queries a Solr server for results.
